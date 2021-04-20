47ef3f84cccc83cf53b34842fa2f109b8f2cfc58
Avishek Sen <x0rzavi@gmail.com>
2021-04-20 13:59:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ const fs = require('fs');
 
 module.exports = {
     session: JSON.parse(process.env.SESSION || fs.readFileSync(__dirname + '/session.json', { encoding: 'utf8' })), //if not using env vars create a file named session.json
+    contact: process.env.CONTACT || "911234567890@c.us",
 }
